c4e6a02fa72ed44ffe0876ac5ed91c251ebe741c
papaj-23 <ladak.pl@gmail.com>
2026-04-09 03:19:29 +0200

    updated submodule of MPU6050 for v1.0 stable. Integrated library with applicaion code

--- STM32L4/Core/Src/callbacks.c
+++ STM32L4/Core/Src/callbacks.c
@@ -5,10 +5,12 @@
 #include "mpu-6050.h"
 
 extern TaskHandle_t mpu6050_data_receive_handle;
+extern MPU_6050_t mpu_handle;
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
     if(htim->Instance == TIM6) {
         if((mpu6050_data_receive_handle != NULL) && (xTaskGetSchedulerState() == taskSCHEDULER_RUNNING)) {
+            //MPU_6050_read_fifo_cnt(&mpu_handle);
             BaseType_t hpw = pdFALSE;
             xTaskNotifyFromISR(mpu6050_data_receive_handle, 0x04U, eSetBits, &hpw);
             portYIELD_FROM_ISR(hpw);
@@ -23,6 +25,7 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 void HAL_I2C_MemRxCpltCallback(I2C_HandleTypeDef *hi2c) {
     if(hi2c == &hi2c1) {
         if((mpu6050_data_receive_handle != NULL) && (xTaskGetSchedulerState() == taskSCHEDULER_RUNNING)) {
+            MPU_6050_i2c_rxcplt_isr(&mpu_handle);
             BaseType_t hpw = pdFALSE;
             xTaskNotifyFromISR(mpu6050_data_receive_handle, 0x02U, eSetBits, &hpw);
             portYIELD_FROM_ISR(hpw);
@@ -33,6 +36,7 @@ void HAL_I2C_MemRxCpltCallback(I2C_HandleTypeDef *hi2c) {
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
     if(GPIO_Pin == GPIO_PIN_12) {
         if((mpu6050_data_receive_handle != NULL) && (xTaskGetSchedulerState() == taskSCHEDULER_RUNNING)) {
+            MPU_6050_int_isr(&mpu_handle);
             BaseType_t hpw = pdFALSE;
             xTaskNotifyFromISR(mpu6050_data_receive_handle, 0x01U, eSetBits, &hpw);
             portYIELD_FROM_ISR(hpw);
--- STM32L4/Core/Src/imu_data_input.c
+++ STM32L4/Core/Src/imu_data_input.c
@@ -5,8 +5,6 @@
 #include "init.h"
 #include "IMU_data_input.h"
 
-#define BURST_COUNT 600
-
 static void MPU6050_data_receive_handler(void* pvParameters);
 
 /* MPU6050 data transfer task */
@@ -24,9 +22,9 @@ static const task_init_t mpu6050_data_receive_init = {
     .tcb = &mpu6050_data_receive_tcb
 };
 
-uint8_t dma_i2c_rx_buf[14];
+uint8_t dma_i2c_rx_buf[14] = {0};
+MPU_6050_rawdata_t inter_buffer;
 
-int16_t intermediate_rx_buf[7];
 MPU_6050_t mpu_handle;
 MPU_6050_selftest_t selftest_results;
 MPU_6050_data_t current_data;
@@ -39,35 +37,30 @@ static void MPU6050_data_receive_handler(void* pvParameters) {
     mpu_handle = (MPU_6050_t) {
         .hi2c = &hi2c1,
         .rx_buffer = dma_i2c_rx_buf,
+        .inter_buffer = &inter_buffer,
         .delay_ms_wrapper = delay_wrapper_ms,
-        .burst_count = BURST_COUNT
     };
     uint32_t events = 0U;
-
-    MPU_6050_Init(&mpu_handle);
-    MPU_6050_Self_Test(&mpu_handle, &selftest_results);
-
-    MPU_6050_Set_Mode(&mpu_handle, MPU_SINGLE_MODE);
-    
+    MPU_6050_init(&mpu_handle);
+    MPU_6050_self_test(&mpu_handle, &selftest_results);
+    MPU_6050_set_lp_wakeup_freq(&mpu_handle, F_40HZ);
+    MPU_6050_set_mode(&mpu_handle, MPU_SINGLE_MODE);
 
     for(;;) {
-        check_registers(&mpu_handle);
-        check_registers(&mpu_handle);
+        //MPU_6050_check_registers(&mpu_handle);
         xTaskNotifyWait(0U, 0xFFFFFFFFU, &events, portMAX_DELAY);
 
         /* external interrupt from MPU6050 */
         if(events & 0x01U) {
-            MPU_6050_Interrupt_Handler(&mpu_handle);
+            
         }
 
         /* i2c dma transfer complete */
         if(events & 0x02U) {
-            MPU_6050_parse_payload(dma_i2c_rx_buf, intermediate_rx_buf);
-            current_data = MPU_6050_payload_to_readable(&mpu_handle, intermediate_rx_buf);
-            //MPU_6050_Process_Burst_Cnt(&mpu_handle);
-            if(mpu_handle.fifo_counter >= BURST_COUNT) {
-                //MPU_6050_Burst_Read(&mpu_handle);
-                mpu_handle.fifo_counter -= BURST_COUNT;
+            MPU_6050_parse_payload(&mpu_handle);
+            current_data = MPU_6050_payload_to_readable(&mpu_handle);
+            if(mpu_handle.fifo_oflow_flag == 1U) {
+                MPU_6050_fifo_reset(&mpu_handle);
             }
         }
 
